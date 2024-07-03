7bfb7a28abff2b9b45ca130ddb7b869aa3972b8c
github-usman <usmantoyou@gmail.com>
2024-07-03 08:28:56 +0530

    Frontend : intialized with create-react-app.

---
+++ frontend/src/components/welcome/WelcomePage.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactTyped } from "react-typed";
+import styles from "./welcomePage.module.css";
+
+
+const WelcomePage = () => {
+  return (
+    <div className='bg-primary vh-100 w-100 border border-black d-flex justify-content-center align-items-center'>
+       <div className="container text-center">
+        <p className="fs-2">Hi there!</p>
+        <h2 className={styles.welcome}>Welcome! to xcelore portal</h2>
+        <h3 >
+          <ReactTyped
+            strings={[
+              "You will Directly Connect to Admin Login",
+              "One Login",
+            ]}
+            typeSpeed={40}
+            backSpeed={50}
+            attr="placeholder"
+            loop
+          >
+            <input type="text" className={styles.reactTypedInner} disabled />
+          </ReactTyped>
+        </h3>
+        <div>
+          <Link to="/login" className={styles.btnGetStarted}>
+            Get started
+          </Link>
+        </div>
+       
+      </div>
+    </div>
+  );
+};
+
+export default WelcomePage;
