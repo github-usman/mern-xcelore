090db1263edaf5f7b71ed45591d52fc8b9a645d2
github-usman <usmantoyou@gmail.com>
2024-07-04 13:53:14 +0530

    update user profile and loading on logging in fix

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -4,6 +4,7 @@ import { SiTicktick } from "react-icons/si";
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { login } from '../store/thunks/authThunk';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
